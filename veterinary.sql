7dd52bb797b2e79fe6d4f834504c8c6d5db391dd
michael070606 <094749568688ty@gmail.com>
2025-11-21 15:51:03 +0800

    feat/create-table-medicalrecords

--- veterinary.sql
+++ veterinary.sql
@@ -10,3 +10,14 @@ CREATE TABLE animals (
     FOREIGN KEY (ownerid)
         REFERENCES owners(ownerid)
 );
+
+CREATE TABLE medicalrecords (
+    recordid INT PRIMARY KEY,
+    animalid INT,
+    doctorid INT,
+    diagnosis VARCHAR(255),
+    treatment VARCHAR(255),
+    recorddate DATE,
+    FOREIGN KEY (animalid) REFERENCES animals(animalid),
+    FOREIGN KEY (doctorid) REFERENCES doctors(doctorid)
+);
