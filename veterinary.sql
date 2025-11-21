569dafebd344d6e50a2b81a0f038bd689bd9112d
michael070606 <094749568688ty@gmail.com>
2025-11-21 15:20:18 +0800

    feat/create-table-invoices

--- veterinary.sql
+++ veterinary.sql
@@ -1,8 +1,17 @@
 CREATE TABLE owners(
-  ownerid INT PRIMARY KEY,
-  ofirstname VARCHAR(50),
-  olastname VARCHAR(50),
-  address VARCHAR(100),
-  phone VARCHAR(15),
-  email VARCHAR(100)
+    ownerid INT PRIMARY KEY,
+    ofirstname VARCHAR(50),
+    olastname VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
+
+
+CREATE TABLE appointments (
+    appointid INT PRIMARY KEY,
+    animalid INT,
+    appointmentdate DATE,
+    reason VARCHAR(255),
+    FOREIGN KEY (animalid) REFERENCES animals(animalid)
 );
