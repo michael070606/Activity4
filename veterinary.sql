b0bdcda98024fe1d6a93dd7592014afc4791fc5e
michael070606 <094749568688ty@gmail.com>
2025-11-21 17:05:53 +0800

    feat/insert-10-rows-invoices

--- veterinary.sql
+++ veterinary.sql
@@ -78,3 +78,17 @@ INSERT INTO animals (animalid, name, species, breed, dateofbirth, gender, color,
 (9, 'Bella', 'Dog', 'Poodle', '2017-06-06', 'Female', 'White', 9),
 (10, 'Oscar', 'Cat', 'Persian', '2020-08-08', 'Male', 'White/Gray', 10);
 
+
+INSERT INTO invoices(invoiceid, appointid, totalamount, paymentdate)
+VALUES
+(1, 1, 50.00, '09:30:00'),
+(2, 2, 75.00, '14:15:00'),
+(3, 3, 100.00, '11:00:00'),
+(4, 4, 200.00, '13:45:00'),
+(5, 5, 80.00, '10:30:00'),
+(6, 6, 30.00, '15:00:00'),
+(7, 7, 75.00, '09:15:00'),
+(8, 8, 150.00, '16:30:00'),
+(9, 9, 60.00, '14:45:00'),
+(10, 10, 40.00, '11:30:00');
+
